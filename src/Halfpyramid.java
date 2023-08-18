fedcb8330458562c3f5494e1617f50bf0ab2124d
stormshaper45 <123022397+stormshaper45@users.noreply.github.com>
2023-08-17 22:43:33 -0400

    Update Halfpyramid.java

--- src/Halfpyramid.java
+++ src/Halfpyramid.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Halfpyramid {
+package PACKAGE_NAME;
+public class Halfpyramid {
+    public static void main(String[] args){
+        String zero = "";
+        for (int i = 0; i <= 4; i++) {
+            zero = (0) + " " + zero;
+            System.out.println(zero);
+        }
+    }
 }
